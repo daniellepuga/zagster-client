0a21ad5a3790845f314bf4884fee898482a634c8
Danielle Puga <danielle.puga@gmail.com>
2019-11-25 13:57:03 -0800

    milestone 2

--- main.js
+++ main.js
@@ -7,23 +7,27 @@ function updateView() {
 }
 
 function updateGraph() {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data : {
-            labels: ['January', 'February', 'March', 'April', 'June', 'July'],
-            database: [{
-                label: 'My first dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-        }
-    })
-    options: {}
-}
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+  
+      // The data for our dataset
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+      },
+  
+      // Configuration options go here
+      options: {}
+  });
 
-function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h2#rideCount").html(numberOfRides)
+// function updateRideCount(data) {
+//   numberOfRides = data.count
+//   $("h2#rideCount").html(numberOfRides)
 }
